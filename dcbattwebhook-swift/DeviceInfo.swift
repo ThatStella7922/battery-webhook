13762a57764624a615f55c35b5753981c2065227
Dhinak G <17605561+dhinakg@users.noreply.github.com>
2023-08-01 09:43:19 +0530

    Add cursed Mac model support
    Also strip spaces from webhook URL

--- dcbattwebhook-swift/DeviceInfo.swift
+++ dcbattwebhook-swift/DeviceInfo.swift
@@ -18,7 +18,48 @@ public extension UIDevice {
             guard let value = element.value as? Int8, value != 0 else { return identifier }
             return identifier + String(UnicodeScalar(UInt8(value)))
         }
+        
+        #if os(macOS) || targetEnvironment(macCatalyst)
+        #if arch(x86_64)
+        
+        // Not tested through the App Sandbox. Cope
 
+        var serialNumber: String? {
+            let service = IOServiceGetMatchingService(kIOMasterPortDefault, IOServiceMatching("IOPlatformExpertDevice"))
+            return IORegistryEntryCreateCFProperty(service, "IOPlatformSerialNumber" as CFString, kCFAllocatorDefault, 0).takeUnretainedValue() as? String
+        }
+
+        var modelName: String? {
+            guard let serial = serialNumber,
+                let plist = try? PropertyList.load(from: .init(fileURLWithPath: "\(NSHomeDirectory())/Library/Preferences/com.apple.SystemProfiler.plist")),
+                let regionCode = Locale.current.regionCode,
+                let names = plist["CPU Names"] as? [String: String],
+                !names.isEmpty else {
+                    return nil
+            }
+            for language in Locale.preferredLanguages {
+                let key = "\(serial.suffix(4))-\(language)_\(regionCode)"
+                if let entry = names[key] {
+                    return entry
+                }
+            }
+            return nil
+        }
+
+        return modelName ?? "Unknown: \(identifier)"
+
+        
+        #else
+        let entry = IORegistryEntryFromPath(kIOMainPortDefault,"IOService:/AppleARMPE/product")
+        defer { IOObjectRelease(entry) }
+        let deviceName = IORegistryEntryCreateCFProperty(entry, "product-name" as CFString, kCFAllocatorDefault, 0)
+        if (deviceName == nil) {
+            return "Unknown: \(identifier)"
+        }
+        return String(decoding: deviceName!.takeUnretainedValue() as! Data, as: UTF8.self)
+        #endif
+        #endif
+        
         func mapToDevice(identifier: String) -> String { // swiftlint:disable:this cyclomatic_complexity
             #if os(iOS)
             switch identifier {
@@ -159,13 +200,7 @@ func isiOSPre16() -> Bool {
 
 /// Returns the device name as a string (ex. `iPad Pro (12.9-inch) (2nd generation)`)
 func getDeviceModel() -> String {
-    #if targetEnvironment(macCatalyst)
-    return "Mac - Stella is lazy and this is Catalyst"
-    
-    #else
     return UIDevice.modelName
-    
-    #endif
 }
 
 /**
