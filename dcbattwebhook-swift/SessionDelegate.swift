96b316a5f7f3fef888d76e2fb91c3795893a7397
Dhinak G <17605561+dhinakg@users.noreply.github.com>
2023-08-01 17:08:57 +0530

    Remove unused functions

--- dcbattwebhook-swift/SessionDelegate.swift
+++ dcbattwebhook-swift/SessionDelegate.swift
@@ -10,35 +10,13 @@ import Foundation
 import WatchConnectivity
 #endif
 
-
-// Custom notifications happen when Watch Connectivity activation or reachability status changes,
-// or when receiving or sending data. Clients observe these notifications to update the UI.
-//
-extension Notification.Name {
-    static let dataDidFlow = Notification.Name("DataDidFlow")
-    static let activationDidComplete = Notification.Name("ActivationDidComplete")
-    static let reachabilityDidChange = Notification.Name("ReachabilityDidChange")
-}
-
 private let defaults = UserDefaults.standard
 
-// Implement WCSessionDelegate methods to receive Watch Connectivity data and notify clients.
-// Handle WCSession status changes.
-//
 class SessionDelegator: NSObject, WCSessionDelegate {
-    
-    // Monitor WCSession activation state changes.
-    //
     func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
-
+        // ok
     }
-    
-    // Monitor WCSession reachability state changes.
-    //
-    func sessionReachabilityDidChange(_ session: WCSession) {
 
-    }
-    
     // Did receive an app context.
     //
     func session(_ session: WCSession, didReceiveApplicationContext applicationContext: [String: Any]) {
@@ -59,20 +37,7 @@ class SessionDelegator: NSObject, WCSessionDelegate {
         self.session(session, didReceiveMessage: message)
         replyHandler(message) // Echo back the time stamp.
     }
-    
-    // Did receive a piece of message data, and the peer doesn't need a response.
-    //
-    func session(_ session: WCSession, didReceiveMessageData messageData: Data) {
 
-    }
-    
-    // Did receive a piece of message data, and the peer needs a response.
-    //
-    func session(_ session: WCSession, didReceiveMessageData messageData: Data, replyHandler: @escaping (Data) -> Void) {
-        self.session(session, didReceiveMessageData: messageData)
-        replyHandler(messageData) // Echo back the time stamp.
-    }
-    
     // Did receive a piece of userInfo.
     //
     func session(_ session: WCSession, didReceiveUserInfo userInfo: [String: Any] = [:]) {
@@ -84,33 +49,16 @@ class SessionDelegator: NSObject, WCSessionDelegate {
     func session(_ session: WCSession, didFinish userInfoTransfer: WCSessionUserInfoTransfer, error: Error?) {
 
     }
-    
-    // Did receive a file.
-    //
-    func session(_ session: WCSession, didReceive file: WCSessionFile) {
 
-    }
-    
-    // Did finish a file transfer.
-    //
-    func session(_ session: WCSession, didFinish fileTransfer: WCSessionFileTransfer, error: Error?) {
-
-    }
-    
     // WCSessionDelegate methods for iOS only.
     //
     #if os(iOS)
     func sessionDidBecomeInactive(_ session: WCSession) {
-        print("\(#function): activationState = \(session.activationState.rawValue)")
+        // ok
     }
     
     func sessionDidDeactivate(_ session: WCSession) {
-        // Activate the new session after having switched to a new watch.
         session.activate()
-    }
-    
-    func sessionWatchStateDidChange(_ session: WCSession) {
-        print("\(#function): activationState = \(session.activationState.rawValue)")
     }
     #endif
 }
