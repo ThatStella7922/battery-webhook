0f081dd9f7522de69d4b9284a913502cd690c6fb
ThatStella7922 <10524728+ThatStella7922@users.noreply.github.com>
2023-08-20 19:35:26 -0600

    Improve EmbedStructs - Rename structs - Add a little documentation

--- dcbattwebhook-swift/EmbedStructs.swift
+++ dcbattwebhook-swift/EmbedStructs.swift
@@ -7,33 +7,75 @@
 
 import Foundation
 
-//Struct for the json contents
-struct MsgAuthor: Codable {
+
+//
+// Discord Structs
+//
+/**
+ Author block to be used in a `DiscordEmbed`
+ 
+ Supply `name` and `icon_url` as strings.
+ [View on Discord Webhooks Guide](https://birdie0.github.io/discord-webhooks-guide/structure/embed/author.html)
+ */
+struct DiscordAuthor: Codable {
     var name: String?
     var icon_url: String?
 }
 
-struct MsgEmbed: Codable {
-    var author: MsgAuthor?
-    var footer: MsgFooter?
+/**
+ DiscordEmbed to be used in a `DiscordMessageObj`.
+ 
+ This object can contain a `DiscordAuthor`, `DiscordFooter`, title, decimal color value, and more. See the struct definition and the link below for more.
+ 
+ You can supply multiple `DiscordEmbed`s in a `DiscordMessageObj` to provide multiple embeds with a single web request.
+ 
+ [View on Discord Webhooks Guide](https://birdie0.github.io/discord-webhooks-guide/structure/embeds.html)
+ */
+struct DiscordEmbed: Codable {
+    var author: DiscordAuthor?
+    var footer: DiscordFooter?
     var title: String?
     var description: String?
     var color: Int?
-    var fields: [MsgField]?
+    var fields: [DiscordEmbedField]?
 }
 
-struct MsgField: Codable {
+/**
+ Field block to be used in a `DiscordEmbed`
+ 
+ [See on Discord Webhooks Guide](https://birdie0.github.io/discord-webhooks-guide/structure/embed/fields.html)
+ */
+struct DiscordEmbedField: Codable {
     var name: String
     var value: String
     var inline: Bool
 }
 
-struct MsgFooter: Codable {
+/**
+ Footer block to be used in a `DiscordEmbed`
+ 
+ Supply `text` and `icon_url` as strings.
+ [View on Discord Webhooks Guide](https://birdie0.github.io/discord-webhooks-guide/structure/embed/footer.html)
+ */
+struct DiscordFooter: Codable {
     var text: String?
     var icon_url: String?
 }
 
-struct MessageObj: Codable {
-    var embeds: [MsgEmbed]?
+/**
+ Footer block to be used in a `DiscordEmbed`
+ 
+ Supply `text` and `icon_url` as strings.
+ [View on Discord Webhooks Guide](https://birdie0.github.io/discord-webhooks-guide/discord_webhook.html)
+ */
+struct DiscordMessageObj: Codable {
+    var embeds: [DiscordEmbed]?
     var contents: String?
+    var username: String? // overrides the predefined username of the webhook
+    var avatar_url: String? // overrides the predefined avatar of the webhook
 }
+
+
+//
+// Structs for other services below
+//
