78673204dc18c25f07ec3002f8a5109ae5905e12
ThatStella7922 <10524728+ThatStella7922@users.noreply.github.com>
2023-11-16 05:57:26 -0500

    Add dummy content to HelpView for now

--- dcbattwebhook-swift/HelpView.swift
+++ dcbattwebhook-swift/HelpView.swift
@@ -14,11 +14,23 @@ struct HelpView: View {
                 WelcomeView().padding(.bottom)
                 Text("To learn more, you can view the help available below.").multilineTextAlignment(.center)
             }
-            List() {
-                NavigationLink(destination: WhatIsAWebhookHelpView(), label: {
-                    Text("What is a webhook?")
-                })
-            }
-        }.navigationTitle("Help")
+            NavigationLink(destination: WhatIsAWebhookHelpView(), label: {
+                Text("What is a webhook?")
+            })
+            NavigationLink(destination: WhatIsAWebhookHelpView(), label: {
+                Text("What's the use-case for something like this?")
+            })
+            NavigationLink(destination: WhatIsAWebhookHelpView(), label: {
+                Text("How do I create a webhook in Discord?")
+            })
+            NavigationLink(destination: WhatIsAWebhookHelpView(), label: {
+                Text("How do I configure Battery Webhook?")
+            })
+        }
+        #if os(macOS)
+        .formStyle(.grouped)
+        #endif
+        .navigationTitle("Help")
+        
     }
 }
