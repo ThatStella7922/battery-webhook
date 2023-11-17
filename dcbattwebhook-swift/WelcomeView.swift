74e95bb73ffa694075f7a37a976f294019de2f5d
ThatStella7922 <10524728+ThatStella7922@users.noreply.github.com>
2023-11-17 04:22:09 -0500

    Add no-icon variant of WelcomeView

--- dcbattwebhook-swift/WelcomeView.swift
+++ dcbattwebhook-swift/WelcomeView.swift
@@ -19,3 +19,11 @@ struct WelcomeView: View {
         Text("To use it, you'll need to configure Battery Webhook with a webhook to push to, as well as filling out some basic info on how you would like to appear to the webhook (such as a display name)").multilineTextAlignment(.center)
     }
 }
+
+struct WelcomeViewNoIcon: View {
+    var body: some View {
+        Text("Welcome to \(prodName)!").font(.title).padding(.bottom).multilineTextAlignment(.center)
+        Text("Battery Webhook is an app that lets you send your battery info to popular services using webhooks.").padding(.bottom).multilineTextAlignment(.center)
+        Text("To use it, you'll need to configure Battery Webhook with a webhook to push to, as well as filling out some basic info on how you would like to appear to the webhook (such as a display name)").multilineTextAlignment(.center)
+    }
+}
