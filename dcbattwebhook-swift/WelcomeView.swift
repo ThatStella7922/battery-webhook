8ec83b1a794f22809c6b324b0a4d5aa8a57d53c4
ThatStella7922 <10524728+ThatStella7922@users.noreply.github.com>
2023-11-19 00:41:56 -0500

    Fix welcome text getting cut off

--- dcbattwebhook-swift/WelcomeView.swift
+++ dcbattwebhook-swift/WelcomeView.swift
@@ -14,7 +14,7 @@ struct WelcomeView: View {
             .aspectRatio(contentMode: .fit)
             .frame(maxWidth: 110)
             .padding()
-        Text("Welcome to \(prodName)!").font(.title).padding(.bottom).multilineTextAlignment(.center)
+        Text("Welcome to \(prodName)!").font(.title).padding(.bottom).multilineTextAlignment(.center).minimumScaleFactor(0.2)
         Text("Battery Webhook is an app that lets you send your battery info to popular services using webhooks.").padding(.bottom).multilineTextAlignment(.center)
         Text("To use it, you'll need to configure Battery Webhook with a webhook to push to, as well as filling out some basic info on how you would like to appear to the webhook (such as a display name)").multilineTextAlignment(.center)
     }
@@ -22,7 +22,7 @@ struct WelcomeView: View {
 
 struct WelcomeViewNoIcon: View {
     var body: some View {
-        Text("Welcome to \(prodName)!").font(.title).padding(.bottom).multilineTextAlignment(.center)
+        Text("Welcome to \(prodName)!").font(.title).padding(.bottom).multilineTextAlignment(.center).minimumScaleFactor(0.2)
         Text("Battery Webhook is an app that lets you send your battery info to popular services using webhooks.").padding(.bottom).multilineTextAlignment(.center)
         Text("To use it, you'll need to configure Battery Webhook with a webhook to push to, as well as filling out some basic info on how you would like to appear to the webhook (such as a display name)").multilineTextAlignment(.center)
     }
