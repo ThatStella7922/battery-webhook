fb4131d6f6c767c70100dace147221ee185fbcb5
ThatStella7922 <10524728+ThatStella7922@users.noreply.github.com>
2024-02-27 03:05:52 -0500

    Move Pkg to its own repo, use BWTest for ensuring the functionality of Pkg

--- BWTest/ViewController.swift
+++ BWTest/ViewController.swift
@@ -6,18 +6,28 @@
 //
 
 import UIKit
-import BatteryWebhook
-import BatteryWebhookCore
+import BatteryWebhookLib
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var versionLabel: UILabel!
+    @IBOutlet weak var errorLabel: UILabel!
     
     
     @IBAction func testNetFunctionButton(_ sender: UIButton) {
         let obj = BatteryWebhookServices.DiscordService.DiscordPayload(content: "gm")
         
-        BatteryWebhookSendInfo.sendDiscord(sendUrl: "https://discord.com/api/webhooks/1031569089157681152/MV9_sStrZbjGQVA_MFdPE2i5a6SBvRTfkZSIig2fB28ADnzYsEankfvJH8VriNTivrBe", payload: obj)
+        do {
+            try BatteryWebhookSendInfo.sendDiscord(sendUrl: "https://discord.com/api/webhooks/1031569089157681152/MV9_sStrZbjGQVA_MFdPE2i5a6SBvRTfkZSIig2fB28ADnzYsEankfvJH8VriNTivrB", payload: obj)
+        } catch BatteryWebhookServices.DiscordService.DiscordNetworkError.intermediary(let errorObj) {
+            print("[ViewController] Discord Network Error: \(errorObj.message) with code \(errorObj.code)")
+            errorLabel.text = "Discord Network Error: \(errorObj.message)\nError code: \(errorObj.code)"
+        } catch BatteryWebhookNetworking.NetworkingError.systemError(let errorText){
+            print("[ViewController] System Error: \(errorText)")
+            errorLabel.text = "[ViewController] System Error: \(errorText)"
+        } catch {
+            print("[ViewController] Unhandled Error: \(error)")
+        }
     }
 
     
